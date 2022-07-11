6f2a5972e0f185a61be161629b070df14f3c2995
Sourav Dutta <duttasourav029@gmail.com>
2022-07-12 01:48:39 +0530

    FactoryHelper and seeding the relational models

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
+use Database\Factories\Helpers\FactoryHelper;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,10 +19,12 @@ class CommentFactory extends Factory
      */
     public function definition()
     {
+        $postId = FactoryHelper::getRandomModelId(Post::class);
+        $userId = FactoryHelper::getRandomModelId(User::class);
         return [
             'body' => [],
-            'user_id' => 1,
-            'post_id' => 1
+            'user_id' => $userId,
+            'post_id' => $postId
         ];
     }
 }
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Post;
+use App\Models\User;
+use Database\Factories\Helpers\FactoryHelper;
 use Database\Seeders\Traits\DisableForeignKeys;
 use Database\Seeders\Traits\TruncateTable;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -22,7 +24,11 @@ class PostSeeder extends Seeder
 
         $this->truncate('posts');
 
-        Post::factory(3)->untitled()->create();
+        $posts = Post::factory(3)->untitled()->create();
+
+        $posts->each(function (Post $post){
+            $post->users()->sync([FactoryHelper::getRandomModelId(User::class)]);
+        });
 
         $this->enableForeignKeys();
     }
